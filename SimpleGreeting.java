403b7cd6e6be144165cad6630a5c90ba15c5b022
dmohle <10038913+dmohle@users.noreply.github.com>
2025-01-12 18:28:03 -0800

    Add files via upload

---
+++ SimpleGreeting.java
@@ -0,0 +1,31 @@
+// simpleGreeting.java
+// dH 1/12/25
+// demo for CIT-63 class (online)
+// refactor to rename the default "Main" to something meaningful to you (the Java programmer)
+
+// Scanner class is here:
+// https://www.w3schools.com/java/java_user_input.asp
+
+import java.util.Scanner;
+
+public class SimpleGreeting {
+    public static void main(String[] args) {
+        System.out.println("\n\n****** Welcome to my Simple Greeting Program! ***** \n\n");
+
+        // Create a scanner object for user input
+        Scanner scanner = new Scanner(System.in);
+
+        // Create a String variable to hold our user's name.
+        String myUserName;
+
+        // Prompt the user for their name.
+        System.out.println("\n Please enter your name: ");
+
+        // get the input into our variable
+        myUserName = scanner.nextLine();
+
+        // Prove that you got the user name.
+        System.out.println("\n Hello " + myUserName + " how are you today?");
+
+    }
+}
